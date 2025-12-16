aef89f75aadb7e5ee00469cefd8da50bc4bc4cf1
yuraKnp <yuraknp@gmail.com>
2025-12-16 19:00:10 +0200

    update custom js

--- assets/custom.js
+++ assets/custom.js
@@ -2,24 +2,41 @@
 jQuery(function ($) {
   "use strict";
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const formContainer = document.querySelector('.contact-form-section');
+    $(document).ready( function() {
 
-        console.log(formContainer);
+        const formContainer = $('.contact-form-section');
 
-        console.log(formContainer.dataset.formStatus);
-        
-        if (formContainer && formContainer.dataset.formStatus === 'success') {
+        if (formContainer && formContainer.attr('data-form-status') === 'success') {
         _functions.openPopup('.popup-content[data-rel="1"]');
 
         
         
-        if (window.history.replaceState) {
-            const cleanUrl = window.location.href.split('?')[0];
-            window.history.replaceState({path: cleanUrl}, '', cleanUrl);
+            if (window.history.replaceState) {
+                const cleanUrl = window.location.href.split('?')[0];
+                window.history.replaceState({path: cleanUrl}, '', cleanUrl);
+            }
         }
-        }
+
     });
+
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     const formContainer = document.querySelector('.contact-form-section');
+
+    //     console.log(formContainer);
+
+    //     console.log(formContainer.dataset.formStatus);
+        
+    //     if (formContainer && formContainer.dataset.formStatus === 'success') {
+    //     _functions.openPopup('.popup-content[data-rel="1"]');
+
+        
+        
+    //     if (window.history.replaceState) {
+    //         const cleanUrl = window.location.href.split('?')[0];
+    //         window.history.replaceState({path: cleanUrl}, '', cleanUrl);
+    //     }
+    //     }
+    // });
   
 
 });
