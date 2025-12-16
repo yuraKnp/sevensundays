6f64d0a4517af79127d0e69fe5751f06a7e514d5
yuraKnp <yuraknp@gmail.com>
2025-12-16 18:42:38 +0200

    update js

--- assets/custom.js
+++ assets/custom.js
@@ -2,26 +2,25 @@
 jQuery(function ($) {
   "use strict";
 
+    document.addEventListener('DOMContentLoaded', function() {
+        const formContainer = document.querySelector('.contact-form-section');
 
+        console.log(formContainer);
+
+        console.log(formContainer.dataset.formStatus);
+        
+        if (formContainer && formContainer.dataset.formStatus === 'success') {
+        _functions.openPopup('.popup-content[data-rel="1"]');
+
+        
+        
+        if (window.history.replaceState) {
+            const cleanUrl = window.location.href.split('?')[0];
+            window.history.replaceState({path: cleanUrl}, '', cleanUrl);
+        }
+        }
+    });
   
 
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const formContainer = document.querySelector('.contact-form-section');
-
-    console.log(formContainer);
-
-    console.log(formContainer.dataset.formStatus);
-    
-    if (formContainer && formContainer.dataset.formStatus === 'success') {
-      _functions.openPopup('.popup-content[data-rel="1"]');
-
-      
-      
-      if (window.history.replaceState) {
-        const cleanUrl = window.location.href.split('?')[0];
-        window.history.replaceState({path: cleanUrl}, '', cleanUrl);
-      }
-    }
-});
