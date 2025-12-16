f7fd6447126af3b8b02cb3c7c2cb44eb1a2a167a
yuraKnp <yuraknp@gmail.com>
2025-12-16 18:34:14 +0200

    update js file

--- assets/custom.js
+++ assets/custom.js
@@ -4,9 +4,15 @@ jQuery(function ($) {
 
   document.addEventListener('DOMContentLoaded', function() {
     const formContainer = document.querySelector('.contact-form-section');
+
+    console.log(formContainer);
+
+    console.log(formContainer.dataset.formStatus);
     
     if (formContainer && formContainer.dataset.formStatus === 'success') {
       _functions.openPopup('.popup-content[data-rel="1"]');
+
+      
       
       if (window.history.replaceState) {
         const cleanUrl = window.location.href.split('?')[0];
